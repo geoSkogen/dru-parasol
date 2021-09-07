413fee00fd458b6110ecd9abfa7a76d8490839be
Joseph Scoggins <borninthecaul@gmail.com>
2021-09-06 21:17:38 -0700

    fixes interminable loop issue with 'generate' resource render

--- src/Controller/ParasolMonologueController.php
+++ src/Controller/ParasolMonologueController.php
@@ -21,15 +21,17 @@ class ParasolMonologueController {
     $element['#source_text'] = [];
 
     for ($i = 0; $i < $paragraphs; $i++) {
+      error_log('RANDOM PHRASES COUNT:');
+      error_log('Paragraph ' . strval($i));
       $this_paragraph = '';
       $random_phrases = mt_rand( round( $phrases/2 ),$phrases);
       $last_number= 0;
       $next_number = 0;
-
+      error_log(strval($random_phrases));
       for ($j = 1; $j < $random_phrases; $j++) {
         do {
           $next_number = floor(mt_rand(0, count($content_arr)-1));
-        } while ( $next_number===$last_number && count($content_arr) );
+        } while ( $next_number===$last_number && count($content_arr) > 1);
 
         $this_paragraph .= $content_arr[$next_number] . ' ';
         $last_number = $next_number;
